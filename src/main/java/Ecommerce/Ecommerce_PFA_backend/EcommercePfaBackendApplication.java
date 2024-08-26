dfa57144ca94764f9be016285fe6566dd3392b8a
loubna <elkarroumiloubna3002@gmail.com>
2024-08-26 16:01:39 +0100

    Initial commit

---
+++ src/main/java/Ecommerce/Ecommerce_PFA_backend/EcommercePfaBackendApplication.java
@@ -0,0 +1,13 @@
+package Ecommerce.Ecommerce_PFA_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePfaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePfaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ecommerce/Ecommerce_PFA_backend/EcommercePfaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Ecommerce.Ecommerce_PFA_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePfaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
